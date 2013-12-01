01567d5f8b2cb4ac10baa66b7e334df5718091f2
wbhicks <wbhicks@users.noreply.github.com>
2013-12-01 10:56:59 -0500

    Added sample comment to Breakout.java
    I added the comment from within GitHub's online editor.

--- Javafiles/Breakout.java
+++ Javafiles/Breakout.java
@@ -19,6 +19,9 @@ public class Breakout extends GraphicsProgram {
 
 /** Width and height of application window in pixels */
 	public static final int APPLICATION_WIDTH = 400;
+	/* Here's a sample comment on the following
+	 * const.
+	 */
 	public static final int APPLICATION_HEIGHT = 600;
 
 /** Dimensions of game board (usually the same) */
